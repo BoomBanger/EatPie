35e6aada7240ad89173da056571ac7331a89d168
MedievalWeapon <80856153+MedievalWeapon@users.noreply.github.com>
2021-12-02 10:50:32 -0500

    Update yummyPie.py

--- yummyPie.py
+++ yummyPie.py
@@ -1,9 +1,10 @@
 import time
-time.sleep(120)
+time.sleep(5)
 
 import datetime
 import git
 import os
+import git
 from tkinter import *
 
 dir = os.path.dirname(os.path.realpath(__file__))
@@ -13,35 +14,47 @@ repo = git.cmd.Git(dir)
 t = -1
 lastpull = 0
 
-def pull():
-  global time
-  try:
-    repo.pull()
-    with f as file.open(os.path.join(dir, "dinnerTime.txt"), "r"):
-      timestr = f.read()
-      #####################################################04/02/22 04:03
-      t = time.mktime(datetime.datetime.strptime(timestr, "%d/%m/%y %H:%M").timetuple())
-   except:
-    pass
-  
-while time.time() < t or t <= 0:
-  if time.time() - lastpull > 4*60*60:
-    pull()
-    lastpull = time.time()
+with open(os.path.join(dir, "dinnerTime.txt"), "r") as f:
+    timestr = f.read()
+    #####################################################04/02/22 04:03
+    t = time.mktime(datetime.datetime.strptime(timestr, "%m/%d/%y %H:%M:%S").timetuple())
 
+print(t)
+
+def pull():
+    global time, dir, repo
+    try:
+        repo.pull()
+        with open(os.path.join(dir, "dinnerTime.txt"), "r") as f:
+            timestr = f.read()
+            #####################################################04/02/22 04:03
+            t = time.mktime(datetime.datetime.strptime(timestr, "%d/%m/%y %H:%M:%S").timetuple())
+    except:
+       pass
+  
+while time.time() < t:
+    print("waiting")
+    if time.time() - lastpull > 4*60*60:
+        pull()
+        lastpull = time.time()
+print("done")
 def updateGif(i, label):
-  try:
-    frame = PhotoImage(file = os.path.join(dir, "raspberry.gif", format = "gif -index %i" %(i)))
-  except:
-    i = 0
-    frame = PhotoImage(file = os.path.join(dir, "raspberry.gif", format = "gif -index %i" %(i)))
-  root.after(100, update, i + 1, label)
+    try:
+        frame = PhotoImage(file = os.path.join(dir, "test.gif"), format = "gif -index %i" %(i))
+        label.configure(image = frame)
+        label.image = frame
+    except:
+        i = 0
+        frame = PhotoImage(file = os.path.join(dir, "test.gif"), format = "gif -index %i" %(i))
+        label.configure(image = frame)
+        label.image = frame
+    root.after(100, updateGif, i + 1, label)
     
 if time.time() < t + 10*60*60:
-  root = Tk()
-  root.attributes("-zoomed", True)
-  label = Label(root)
-  label.pack()
-  root.after(0, updateGif, 0, label)
-  root.attributes("-fullscreen", True)
-  root.mainloop()
+    root = Tk()
+    #root.attributes("-zoomed", True)
+    label = Label(root)
+    label.pack()
+    root.after(0, updateGif, 0, label)
+    #root.attributes("-fullscreen", True)
+    root.mainloop()
