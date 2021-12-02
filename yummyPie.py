dc0500eb51390d1cbf18547d4c27a37ed2c2ac2a
MedievalWeapon <80856153+MedievalWeapon@users.noreply.github.com>
2021-12-02 08:32:17 -0500

    Update yummyPie.py

--- yummyPie.py
+++ yummyPie.py
@@ -1,5 +1,30 @@
 import time
-time.sleep(20)
+time.sleep(120)
 
 import datetime
+import git
+import os
 
+dir = os.path.dirname(os.path.realpath(__file__))
+
+repo = git.cmd.Git(dir)
+
+t = -1
+lastpull = 0
+
+def pull():
+  global time
+  try:
+    repo.pull()
+    with f as file.open(os.path.append(dir, "dinnerTime.txt"), "r"):
+      timestr = f.read()
+      #####################################################30/02/22 04:03
+      t = time.mktime(datetime.datetime.strptime(timestr, "%d/%m/%y %H:%M").timetuple())
+   except:
+    pass
+  
+while time.time() < t or t <= 0:
+  if time.time() - lastpull > 4*60*60:
+    pull()
+    lastpull = time.time()
+  
